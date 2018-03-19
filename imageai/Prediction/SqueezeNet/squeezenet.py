efedb6c257cc1e20a2ecde0a070fd69d14fe7f6b
MOSES OLAFENWA <guymodscientist@gmail.com>
2018-03-20 00:30:56 +0100

    Add files via upload

---
+++ imageai/Prediction/SqueezeNet/squeezenet.py
@@ -0,0 +1,122 @@
+from .imagenet_utils import _obtain_input_shape
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.layers import Input, Convolution2D, MaxPooling2D, Activation, concatenate, Dropout
+from tensorflow.python.keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D
+from tensorflow.python.keras.models import Model
+sq1x1 = "squeeze1x1"
+exp1x1 = "expand1x1"
+exp3x3 = "expand3x3"
+relu = "relu_"
+
+# Modular function for Fire Node
+
+def fire_module(x, fire_id, squeeze=16, expand=64):
+    s_id = 'fire' + str(fire_id) + '/'
+
+    if K.image_data_format() == 'channels_first':
+        channel_axis = 1
+    else:
+        channel_axis = 3
+    
+    x = Convolution2D(squeeze, (1, 1), padding='valid', name=s_id + sq1x1)(x)
+    x = Activation('relu', name=s_id + relu + sq1x1)(x)
+
+    left = Convolution2D(expand, (1, 1), padding='valid', name=s_id + exp1x1)(x)
+    left = Activation('relu', name=s_id + relu + exp1x1)(left)
+
+    right = Convolution2D(expand, (3, 3), padding='same', name=s_id + exp3x3)(x)
+    right = Activation('relu', name=s_id + relu + exp3x3)(right)
+
+    x = concatenate([left, right], axis=channel_axis, name=s_id + 'concat')
+    return x
+
+
+# Original SqueezeNet from paper.
+
+def SqueezeNet(include_top=True, weights='imagenet',
+               input_tensor=None, input_shape=None,
+               pooling=None,
+               classes=1000, model_path=""):
+    """Instantiates the SqueezeNet architecture.
+    """
+    
+
+        
+    if weights not in {'imagenet', None}:
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization) or `imagenet` '
+                         '(pre-training on ImageNet).')
+
+    if weights == 'imagenet' and classes != 1000:
+        raise ValueError('If using `weights` as imagenet with `include_top`'
+                         ' as true, `classes` should be 1000')
+
+
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=227,
+                                      min_size=48,
+                                      data_format=K.image_data_format(),
+                                      require_flatten=include_top)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+
+    x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(img_input)
+    x = Activation('relu', name='relu_conv1')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
+
+    x = fire_module(x, fire_id=2, squeeze=16, expand=64)
+    x = fire_module(x, fire_id=3, squeeze=16, expand=64)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
+
+    x = fire_module(x, fire_id=4, squeeze=32, expand=128)
+    x = fire_module(x, fire_id=5, squeeze=32, expand=128)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
+
+    x = fire_module(x, fire_id=6, squeeze=48, expand=192)
+    x = fire_module(x, fire_id=7, squeeze=48, expand=192)
+    x = fire_module(x, fire_id=8, squeeze=64, expand=256)
+    x = fire_module(x, fire_id=9, squeeze=64, expand=256)
+    
+    if include_top:
+        # It's not obvious where to cut the network... 
+        # Could do the 8th or 9th layer... some work recommends cutting earlier layers.
+    
+        x = Dropout(0.5, name='drop9')(x)
+
+        x = Convolution2D(classes, (1, 1), padding='valid', name='conv10')(x)
+        x = Activation('relu', name='relu_conv10')(x)
+        x = GlobalAveragePooling2D()(x)
+        x = Activation('softmax', name='loss')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling=='max':
+            x = GlobalMaxPooling2D()(x)
+        elif pooling==None:
+            pass
+        else:
+            raise ValueError("Unknown argument for 'pooling'=" + pooling)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    inputs = img_input
+
+    model = Model(inputs, x, name='squeezenet')
+
+    # load weights
+    if weights == 'imagenet':
+        weights_path = model_path
+
+        model.load_weights(weights_path)
+
+        
+    return model
+
+
