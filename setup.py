a5b00489894461c9b7473c9e9ee247f99670c7d7
MOSES OLAFENWA <guymodscientist@gmail.com>
2018-03-20 00:17:32 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(name="imageai",
+      version='1.0.1',
+      description='A flexible image prediction library for applications and systems',
+      url="https://moses.specpal.science",
+      author='Moses Olafenwa and John Olafenwa',
+      license='MIT',
+      packages= find_packages(),
+      zip_safe=False
+
+      )
