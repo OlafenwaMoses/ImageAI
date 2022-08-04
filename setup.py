2dda163948aac109845ee33868e28752db65823e
cdud99 <91852023+cdud99@users.noreply.github.com>
2022-08-03 23:10:10 -0700

    Update setup.py
    Reflects working packages at the time of development

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup,find_packages
 
 setup(name="imageai",
-      version='2.1.6',
-      description='A python library built to empower developers to build applications and systems  with self-contained Computer Vision capabilities',
+      version='2.2.0',
+      description='A python library built to empower developers to build applications and systems with self-contained Computer Vision capabilities',
       url="https://github.com/OlafenwaMoses/ImageAI",
       author='Moses Olafenwa and John Olafenwa',
       author_email='guymodscientist@gmail.com',
       license='MIT',
       packages= find_packages(),
-      install_requires=['numpy==1.19.3','scipy==1.4.1','pillow==8.1.1',"matplotlib==3.3.2", "h5py==2.10.0", "keras-resnet==0.2.0", "opencv-python", "keras==2.4.3"],
+      install_requires=['tensorflow==2.9.1', 'keras==2.9.0', 'numpy==1.23.1', 'pillow==8.4.0', 'scipy==1.9.0', 'h5py==3.7.0', 'matplotlib==3.5.2', 'opencv-python==4.6.0.66', 'keras-resnet==0.2.0'],
       zip_safe=False
-
       )
