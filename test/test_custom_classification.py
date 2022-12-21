5ae3554d44c4e94c0794465f7877474117f313fa
OlafenwaMoses <guymodscientist@gmail.com>
2022-12-21 10:27:56 +0000

    fix broken custom classification test by updating json mapping path

--- test/test_custom_classification.py
+++ test/test_custom_classification.py
@@ -23,7 +23,7 @@ def test_recognition_model_mobilenetv2(image_input):
     classifier = CustomImageClassification()
     classifier.setModelTypeAsMobileNetV2()
     classifier.setModelPath(os.path.join(test_folder, "data-models", "mobilenet_v2-idenprof-test_acc_0.85300_epoch-92.pt"))
-    classifier.setJsonPath(os.path.join(test_folder, "data-models", "idenprof_model_classes.json"))
+    classifier.setJsonPath(os.path.join(test_folder, "data-json", "idenprof_model_classes.json"))
     classifier.loadModel()
     predictions, probabilities = classifier.classifyImage(image_input=image_input, result_count=5)
 
@@ -46,7 +46,7 @@ def test_recognition_model_resnet(image_input):
     classifier = CustomImageClassification()
     classifier.setModelTypeAsResNet50()
     classifier.setModelPath(os.path.join(test_folder, "data-models", "resnet50-idenprof-test_acc_0.78200_epoch-91.pt"))
-    classifier.setJsonPath(os.path.join(test_folder, "data-models", "idenprof_model_classes.json"))
+    classifier.setJsonPath(os.path.join(test_folder, "data-json", "idenprof_model_classes.json"))
     classifier.loadModel()
     predictions, probabilities = classifier.classifyImage(image_input=image_input, result_count=5)
 
@@ -68,7 +68,7 @@ def test_recognition_model_inceptionv3(image_input):
     classifier = CustomImageClassification()
     classifier.setModelTypeAsInceptionV3()
     classifier.setModelPath(os.path.join(test_folder, "data-models", "inception_v3-idenprof-test_acc_0.81050_epoch-92.pt"))
-    classifier.setJsonPath(os.path.join(test_folder, "data-models", "idenprof_model_classes.json"))
+    classifier.setJsonPath(os.path.join(test_folder, "data-json", "idenprof_model_classes.json"))
     classifier.loadModel()
     predictions, probabilities = classifier.classifyImage(image_input=image_input, result_count=5)
 
@@ -90,7 +90,7 @@ def test_recognition_model_densenet(image_input):
     classifier = CustomImageClassification()
     classifier.setModelTypeAsDenseNet121()
     classifier.setModelPath(os.path.join(test_folder, "data-models", "densenet121-idenprof-test_acc_0.82550_epoch-95.pt"))
-    classifier.setJsonPath(os.path.join(test_folder, "data-models", "idenprof_model_classes.json"))
+    classifier.setJsonPath(os.path.join(test_folder, "data-json", "idenprof_model_classes.json"))
     classifier.loadModel()
     predictions, probabilities = classifier.classifyImage(image_input=image_input, result_count=5)
 
